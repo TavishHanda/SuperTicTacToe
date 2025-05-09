6d117aa754201ae02c33a63b77441ec50aa32506
TavishHanda <149899926+TavishHanda@users.noreply.github.com>
2025-05-09 03:06:58 -0600

    got tailwind working

--- super-tictactoe/client/src/App.jsx
+++ super-tictactoe/client/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
-import GameRoom from "./components/GameRoom";
+//import GameRoom from "./components/game";
+import './index.css';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/game" element={<GameRoom />} />
+                {/* <Route path="/game" element={<GameRoom />} /> */}
             </Routes>
         </BrowserRouter>
     );
---
+++ super-tictactoe/client/src/components/LandingPage.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
+import { useNavigate } from 'react-router-dom'
+
+function LandingPage() {
+  const [roomCode, setRoomCode] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+
+    socket.on('connect', () => {
+      console.log('Connected to server:', socket.id);
+    })
+
+    return () => socket.disconnect();
+  })
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-6 bg-gray-900">
+      <h1 className="text-5xl text-red-500 font-bold"> Super Tic Tac Shiv</h1>
+
+      <button className="bg-blue-600 px-6 py-3 rounded-log text-log hover:bg-blue-700 transition"
+        onClick={() => {
+          const newCode = Math.random().toString(36).substring(2, 8);
+          navigate(`/game/${newCode}`); // Navigate to the game room with the new code
+        }}
+      >
+        Create Game
+      </button>
+
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          value={roomCode}
+          onChange={(e) => setRoomCode(e.target.value)}
+          placeholder="Enter room code"
+          className="px-4 py-2 border border-gray-300 rounded-lg"
+        />
+        <button className="bg-green-600 px-6 py-3 rounded-log text-log hover:bg-green-700 transition"
+          onClick={() => {
+            if (roomCode.trim() !== "") {
+              // Navigate to the game room with the entered code
+              navigate(`/game/${roomCode}`);
+            }
+          }}
+        >
+          Join Game
+        </button>
+        </div>
+    </div>
+  );
+}
+
+export default LandingPage
